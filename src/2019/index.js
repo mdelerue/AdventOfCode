1e74d16bc6d83f184182bfa45e2caf4fbf307bfc
mdelerue <mdelerue@gmail.com>
2019-12-03 20:12:18 +0100

    rewrite call and result display

--- src/2019/index.js
+++ src/2019/index.js
@@ -2,18 +2,27 @@ import { day01 } from './day01/day01.js';
 import { day02 } from './day02/day02.js';
 import { day03 } from './day03/day03.js';
 
+const loggerStart = (label) => {
+  console.group(label);
+  console.time(label);
+};
+
+const loggerEnd = (label, result) => {
+  console.log('%s', result);
+  console.timeEnd(label);
+  console.groupEnd();
+};
+
+const launch = async (call, label) => {
+  loggerStart(label);
+  const result = await call();
+  loggerEnd(label, result);
+};
+
 const aoc = async () => {
-  const day01Result = await day01();
-  console.log('Day 1 : ');
-  console.log(day01Result);
-
-  const day02Result = await day02();
-  console.log('Day 2 : ');
-  console.log(day02Result);
-
-  const day03Result = await day03();
-  console.log('Day 3 : ');
-  console.log(day03Result);
+  await launch(day01, 'Day 01');
+  await launch(day02, 'Day 02');
+  await launch(day03, 'Day 03');
 };
 
 aoc();
