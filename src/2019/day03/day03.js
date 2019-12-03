6cb612a7005690b5bd140e808fdfccae49a9108f
Marc Delerue <marc.delerue.ext@melting-point.fr>
2019-12-03 17:21:33 +0100

    day 03 - part 2

--- src/2019/day03/day03.js
+++ src/2019/day03/day03.js
@@ -8,20 +8,15 @@ export const day03 = async () => {
     line.split(',')
   );
 
-  const line1Dots = dedupe(
-    getDotCoordinate(line1).map(({ x, y }) => `${x}_${y}`)
-  );
-  const line2Dots = dedupe(
-    getDotCoordinate(line2).map(({ x, y }) => `${x}_${y}`)
-  );
+  const fullCoordLine1 = getDotCoordinate(line1);
+  const fullCoordLine2 = getDotCoordinate(line2);
+  const line1Dots = dedupe(fullCoordLine1.map(({ x, y }) => `${x}_${y}`));
+  const line2Dots = dedupe(fullCoordLine2.map(({ x, y }) => `${x}_${y}`));
 
   const intersection = intersect(line1Dots, line2Dots);
 
   const part1 = intersection
-    .map((item) => ({
-      x: parseInt(item.split('_')[0]),
-      y: parseInt(item.split('_')[1]),
-    }))
+    .map((item) => generateDotFromStringCoord(item))
     .reduce((result, current) => {
       if (!result || Math.abs(current.x) + Math.abs(current.y) < result) {
         return Math.abs(current.x) + Math.abs(current.y);
@@ -29,8 +24,20 @@ export const day03 = async () => {
       return result;
     }, null);
 
+  const part2 = intersection
+    .map((item) => generateDotFromStringCoord(item))
+    .reduce((result, current) => {
+      const delay =
+        fullCoordLine1.find((dot) => dot.x === current.x && dot.y === current.y)
+          .numberOfMove +
+        fullCoordLine2.find((dot) => dot.x === current.x && dot.y === current.y)
+          .numberOfMove;
+      return !result || delay < result ? delay : result;
+    }, null);
+
   return {
     part1,
+    part2,
   };
 };
 
@@ -38,7 +45,9 @@ const getDotCoordinate = (line) =>
   // let's assume that the start line is at coordinate (0,0)
   line.reduce((fullArray, instruction) => {
     const numberOfMovement = parseInt(instruction.slice(1));
-    const start = fullArray.length ? fullArray.slice(-1)[0] : { x: 0, y: 0 };
+    const start = fullArray.length
+      ? fullArray.slice(-1)[0]
+      : { x: 0, y: 0, numberOfMove: 0 };
     const movements = Array.from(Array(numberOfMovement).keys()).map(
       (key) => key + 1
     );
@@ -47,25 +56,53 @@ const getDotCoordinate = (line) =>
     switch (instruction[0]) {
       case 'D':
         partialMoves = movements.reduce(
-          (acc, value) => [...acc, { ...start, y: start.y - value }],
+          (acc, value) => [
+            ...acc,
+            {
+              ...start,
+              y: start.y - value,
+              numberOfMove: start.numberOfMove + value,
+            },
+          ],
           []
         );
         break;
       case 'U':
         partialMoves = movements.reduce(
-          (acc, value) => [...acc, { ...start, y: start.y + value }],
+          (acc, value) => [
+            ...acc,
+            {
+              ...start,
+              y: start.y + value,
+              numberOfMove: start.numberOfMove + value,
+            },
+          ],
           []
         );
         break;
       case 'L':
         partialMoves = movements.reduce(
-          (acc, value) => [...acc, { ...start, x: start.x - value }],
+          (acc, value) => [
+            ...acc,
+            {
+              ...start,
+              x: start.x - value,
+              numberOfMove: start.numberOfMove + value,
+            },
+          ],
           []
         );
         break;
       case 'R':
         partialMoves = movements.reduce(
-          (acc, value) => [...acc, { ...start, x: start.x + value }],
+          (acc, value) => [
+            ...acc,
+            {
+              ...start,
+              x: start.x + value,
+              numberOfMove: start.numberOfMove + value,
+            },
+          ],
           []
         );
         break;
@@ -80,3 +117,8 @@ const intersect = (array1, array2) => {
   var set2 = new Set(array2);
   return [...new Set(array1)].filter((x) => set2.has(x));
 };
+
+const generateDotFromStringCoord = (item) => ({
+  x: parseInt(item.split('_')[0]),
+  y: parseInt(item.split('_')[1]),
+});
