9e9746a33b98724e5bcafdaacc83e87c0d045b25
mdelerue <mdelerue@gmail.com>
2019-12-05 19:53:29 +0100

    day 4. With a bit of cheat for part 2

--- src/2019/index.js
+++ src/2019/index.js
@@ -1,6 +1,7 @@
 import { day01 } from './day01/day01.js';
 import { day02 } from './day02/day02.js';
 import { day03 } from './day03/day03.js';
+import { day04 } from './day04/day04.js';
 
 const loggerStart = (label) => {
   console.group(label);
@@ -23,6 +24,7 @@ const aoc = async () => {
   await launch(day01, 'Day 01');
   await launch(day02, 'Day 02');
   await launch(day03, 'Day 03');
+  await launch(day04, 'Day 04');
 };
 
 aoc();
